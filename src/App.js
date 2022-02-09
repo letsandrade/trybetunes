f8fde74811e8395582e9b65ed05383b94c15cfc5
leticiaandrade <leticia933@gmail.com>
2022-02-09 04:00:16 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -14,13 +14,15 @@ class App extends React.Component {
       <div className="main-div">
         <p>TrybeTunes</p>
         <BrowserRouter>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
         </BrowserRouter>
 
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,77 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends Component {
+  state = {
+    userName: '',
+    isLoading: false,
+    isButtonDisabled: true,
+    toRedirect: false,
+  }
+
+  handleInputChange = ({ target }) => {
+    // console.log('o que esta voltando do input change', target.value);
+    const { name, value } = target;
+    // console.log(target);
+    this.setState({
+      [name]: value,
+    }, this.validateName);
+    // console.log(`mockuser ${name}:${value}`);
+  }
+
+  validateName = () => {
+    const { userName } = this.state;
+    const minSize = 3;
+    if (userName.length >= minSize) {
+      this.setState({
+        isButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isButtonDisabled: true,
+      });
+    }
+  }
+
+  handleLoginButton = async (event) => {
+    event.preventDefault();
+    this.setState({ isLoading: true });
+    const { userName } = this.state;
+    const result = await createUser({ name: userName });
+    if (result === 'OK') {
+      this.setState({
+        isLoading: false,
+        toRedirect: true,
+      });
+    }
+  }
+
   render() {
+    const { userName, isButtonDisabled, isLoading, toRedirect } = this.state;
+    if (isLoading === true) return <Loading />;
+    if (toRedirect === true) return <Redirect to="/search" />;
     return (
       <div data-testid="page-login">
-        login page
+        <form>
+          <input
+            type="text"
+            name="userName"
+            value={ userName }
+            onChange={ this.handleInputChange }
+            data-testid="login-name-input"
+          />
+          <button
+            type="submit"
+            name="LoginButton"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+            onClick={ this.handleLoginButton }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
