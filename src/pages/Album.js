50f283065cc668aab13140b02a01127b6ce12bb2
leticiaandrade <leticia933@gmail.com>
2022-02-09 00:38:55 -0300

    requisito 1

---
+++ src/pages/Album.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        album page
+      </div>
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        not found page
+      </div>
+    );
+  }
+}
+
+export default NotFound;
