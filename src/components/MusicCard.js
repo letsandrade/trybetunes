c8f7292b344c17140deef70e5dbafa7e77223983
leticiaandrade <leticia933@gmail.com>
2022-02-14 02:32:17 -0300

    depois de 84 anos consegui resolver esse requisito amém

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 
 class MusicCard extends Component {
   render() {
+    const { songs } = this.props;
     return (
-      <p>music card</p>
+      <div className="music-card-container">
+        {songs.map((song, index) => {
+          if (song.previewUrl) {
+            return (
+              <section key={ index }>
+                <p className="songTitle">{ song.trackName }</p>
+                <audio data-testid="audio-component" src={ song.previewUrl } controls>
+                  <track kind="captions" />
+                  O seu navegador não suporta o elemento
+                  {' '}
+                  <code>audio</code>
+                  .
+                </audio>
+
+              </section>
+            );
+          }
+          return null;
+        })}
+      </div>
     );
   }
 }
+// inicialmente estava fazendo o map no comp. album, mas tava dando errado pq renderizava sempre um componente a mais. decidi fazer no card, porem tava dando erro, foi onde entendi que tava pegando um elemento sem a propriedade previewurl, peguei a ideia de fazer a condicional com retorno null no pr do rafael
 
 export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
-// import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {
+    artist: '',
+    album: '',
+    songs: [],
+  }
+
+  componentDidMount = () => this.getSongsFromAPI()
+
+  getSongsFromAPI = async () => {
+    const { match: { params: { id } } } = this.props;
+    const foundSongs = await getMusics(id);
+    console.log('response from api', foundSongs);
+    this.setState({
+      songs: foundSongs,
+      artist: foundSongs[0].artistName,
+      album: foundSongs[0].collectionName,
+    });
+  }
+
   render() {
+    const { artist, album, songs } = this.state;
+    console.log('songs in state array', songs);
     return (
       <div data-testid="page-album">
         <Header />
-        <p>album page</p>
-        <h3 data-testid="artist-name">artista</h3>
-        <h4 data-testid="album-name">album</h4>
+        <div>
+          <h3 data-testid="artist-name">{ artist }</h3>
+          <h4 data-testid="album-name">{ album }</h4>
+          <MusicCard songs={ songs } />
+        </div>
       </div>
     );
   }
 }
 
+// ref match: https://v5.reactrouter.com/web/api/match
 export default Album;
